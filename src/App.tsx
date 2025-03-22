b5c7fed825c1308c917dce4a36f3260fdc35b44e
Finn Mao <loverainys@gmail.com>
2025-03-22 13:46:10 +0800

    chore: install and test chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@chakra-ui/react";
+
 function App() {
-  return <>My Todo List</>;
+  return (
+    <>
+      My Todo List
+      <Button>測試</Button>
+    </>
+  );
 }
 
 export default App;
