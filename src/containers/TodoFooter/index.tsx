a8a839d941e8eb451603053d851e92d1e9c47cae
Finn Mao <loverainys@gmail.com>
2025-03-22 14:12:21 +0800

    feat: add TodoFooter styles

--- src/containers/TodoFooter/index.tsx
+++ src/containers/TodoFooter/index.tsx
@@ -1,5 +1,14 @@
+import { Flex, Text } from "@chakra-ui/react";
+
 const TodoFooter = () => {
-  return <div>Footer</div>;
+  return (
+    <Flex justifyContent="space-between">
+      <Text>個待完成項目</Text>
+      <Text color="gray" _hover={{ cursor: "pointer" }}>
+        清除已完成項目
+      </Text>
+    </Flex>
+  );
 };
 
 export default TodoFooter;
