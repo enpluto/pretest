8d67b2989f33cc1479005ba18b622877a36543a5
Finn Mao <loverainys@gmail.com>
2025-03-22 14:05:08 +0800

    feat: add TodoInput styles

--- src/containers/TodoInput/index.tsx
+++ src/containers/TodoInput/index.tsx
@@ -1,5 +1,15 @@
+import { Button, Flex, Input } from "@chakra-ui/react";
+import { useState } from "react";
+
 const TodoInput = () => {
-  return <div>Input</div>;
+  const [text, setText] = useState("");
+
+  return (
+    <Flex gap={2}>
+      <Input placeholder="輸入待辦事項" value={text} />
+      <Button disabled={!text}>新增</Button>
+    </Flex>
+  );
 };
 
 export default TodoInput;
